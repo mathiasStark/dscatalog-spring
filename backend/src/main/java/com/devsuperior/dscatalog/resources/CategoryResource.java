b1bb83ccd949919fe476c1796860cdb6d2a61ef4
mathiasStark <pgo.mathias@gmail.com>
2023-07-29 09:14:01 -0300

    feat: add connection  to h2 database

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,24 +1,30 @@
-package com.devsuperior.dscatalog.resource;
+package com.devsuperior.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dscatalog.entity.Category;
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
+
+/**
+ * Também chamado de Controller
+ */
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
+	
+	@Autowired
+	private CategoryService categoryService;
 
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Eletronics"));
+		List<Category> list = categoryService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
